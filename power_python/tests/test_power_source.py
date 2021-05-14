17fac2b780e8c615e89c3a0733adbd63b471addd
Juan Fernando Donadio <jf.donadio@gmail.com>
2021-05-13 21:54:45 -0300

    Added tests for power_source

--- power_python/tests/test_power_source.py
+++ power_python/tests/test_power_source.py
@@ -1,12 +1,40 @@
+import pytest
+
 from ..power_source import PowerSource
+from ..power_consumer import PowerConsumer
 from ..power_bank import PowerBank
 
 
-def test_example():
-    p = PowerSource(300)
-    assert p.output() == 300
-    assert p.supply_power() == p.output()
-    b = PowerBank(150, 150, 3000)
-    assert b.capacity() == 3000 and b.input() == 150 and b.output() == 0
-    assert p.connect(b) and not p.connect(b)
-    assert p.supply_power() == p.output() - b.input() and b.output() == 150
+def test_init():
+    ps = PowerSource(2000)
+
+    assert isinstance(ps, PowerSource)
+
+def test_output():
+    output = 2000
+    output_different = 3000
+    ps = PowerSource(2000)
+
+    assert ps.output() == output
+    assert ps.output() != str(output)
+    assert ps.output() != output_different
+
+def test_connect():
+    ps = PowerSource(2000)
+    pc = PowerConsumer(100)
+
+    assert ps.connect(pc)
+    assert not ps.connect(pc)
+    
+def test_supply_power():
+    ps = PowerSource(2000)
+    pb = PowerBank(100, 2000, 10000)
+    ps.connect(pb)
+
+    with pytest.raises(NotImplementedError):
+        ps_temp = PowerSource(2000)
+        pc_temp = PowerConsumer(100)
+        ps_temp.connect(pc_temp)
+        ps_temp.supply_power()
+
+    assert ps.supply_power() == 1900
