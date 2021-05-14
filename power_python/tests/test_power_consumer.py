856662450adf24d733408b808c2654f418677e8f
Juan Fernando Donadio <jf.donadio@gmail.com>
2021-05-13 21:54:59 -0300

    Added tests for power_consumer

---
+++ power_python/tests/test_power_consumer.py
@@ -0,0 +1,28 @@
+import pytest
+
+from ..power_source import PowerSource
+from ..power_consumer import PowerConsumer
+from ..power_bank import PowerBank
+
+def test_init():
+    pc = PowerConsumer(100)
+
+    assert isinstance(pc, PowerConsumer)
+
+def test_input():
+    input = 100
+    input_different = 200
+    pc = PowerConsumer(100)
+
+    assert pc.input() == input
+    assert pc.input() != str(input)
+    assert pc.input() != input_different
+
+def test_receive_power():
+    with pytest.raises(NotImplementedError):
+        pc = PowerConsumer(100)
+        pc.receive_power(1)
+
+    with pytest.raises(TypeError):
+        pc = PowerConsumer(100)
+        pc.receive_power()  # pylint: disable=no-value-for-parameter
